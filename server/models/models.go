340e946a4e128fd9f6a82031671e8df8a3db83a6
ahmadexe <muahmad710@gmail.com>
2023-07-01 03:06:18 +0500

    Initial commit

---
+++ server/models/models.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Movie struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name   string             `json:"name,omitempty" bson:"name,omitempty"`
+	Rating float64            `json:"rating,omitempty" bson:"rating,omitempty"`
+	Watched bool			  `json:"watched,omitempty" bson:"watched,omitempty"`
+}

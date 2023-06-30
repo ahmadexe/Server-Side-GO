5eee6c3af8d27d8c08ebc81bcc8c8e42ef1a94bf
ahmadexe <muahmad710@gmail.com>
2023-07-01 04:46:02 +0500

    -terminate

--- server/main.go
+++ server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main()  {
-	
+import (
+	"backend-api/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting server...")
+	log.Fatal(http.ListenAndServe(":8080", router.Router()))
+	fmt.Println("Server running on port 8080")
 }
